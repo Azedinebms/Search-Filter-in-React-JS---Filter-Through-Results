6a2f352b5dfa823f2fcb26d9ecbeafb5fc1223b3
Azedinebms <bamoussa.azedine@gmail.com>
2023-04-11 22:28:51 +0000

    add button add update delete

--- src/App.js
+++ src/App.js
@@ -3,18 +3,47 @@ import './App.css'
 
 const App = () => {
 
-  const [fruits_list, setFruits] = useState(["Apple", "Orange", "Banana"])
+  const [emps,setEmps]=useState([
+    {name:"Raja",experience:"10+ Years"},
+    {name:"Mano",experience:"2 Years"},
+    {name:"Tom",experience:"5+ Years"},
+])
+  const fruit = emps.map((employe, index) => (
+    <p ket={index}><b>{employe.name}  {employe.experience}</b></p>
+  )
+  )
 
-  const fruit = fruits_list.map((fruit, index) => (
-    <p ket={index}><b>{fruit}</b></p>
-  )
-  )
+  const addRow=()=>{
+    let newEmp={name:"Random User1",experience:"6 Years"}
+    setEmps([...emps,newEmp])
+}
+
+const updateRow=()=>{
+  let index=0
+  let newEmp=emps[index]
+  newEmp["name"]="Modfied User";
+  emps[index]=newEmp
+  setEmps([...emps])
+}
+
+const deleteRow = () => {
+  //let name="Mano"
+  //setEmps(emps.filter(emp => emp.name !== name))
+  let copy_emp=[...emps]
+  copy_emp.splice(0,1)
+  setEmps(copy_emp)
+}
+  
 
 
   return (
     <div className='App'>
       <h1>Array with React Js</h1>
       {fruit}
+
+      <button onClick={addRow}>Add</button>
+      <button onClick={updateRow}>Update</button>
+      <button onClick={deleteRow}>Delete</button>
     </div>
   );
 }
